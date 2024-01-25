11bb1ad3bb239c94b9c0fd9001bb4ebe879ceaac
megha saxena <meghasaxena9@gmail.com>
2024-01-25 11:49:56 -0600

    message

--- Chapter-1/codingevents.zip/src/main/java/org/launchcode/codingevents/zip/controllers/EventController.java
+++ Chapter-1/codingevents.zip/src/main/java/org/launchcode/codingevents/zip/controllers/EventController.java
@@ -1,36 +1,58 @@
 package org.launchcode.codingevents.zip.controllers;
+
+import org.launchcode.codingevents.zip.data.EventData;
+import org.launchcode.codingevents.zip.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
-@Controller
-@RequestMapping("events")
+import static org.launchcode.codingevents.zip.data.EventData.events;
+
+@Controller@RequestMapping("templates/events/events/events")
 public class EventController {
 
-        @GetMapping("/index")
-        public String getIndexTemplate(Model model) {
-            List<String> eventNames = new ArrayList<>();
-            eventNames.add("Event 1");
-            eventNames.add("Event 2");
-            eventNames.add("Event 3");
 
+    @GetMapping
+    public String displayAllEvents(Model model) {
+        model.addAttribute("title", "All Events");
+        model.addAttribute("events", EventData.getAll());
+        return "events/index";
+    }
 
-            model.addAttribute("eventNames", eventNames);
+    // lives at /events/create
+    @GetMapping("create")
+    public String displayCreateEventsForm(Model model) {
+        model.addAttribute( "Title","Create Event");
+        return "events/create";
+    }
 
+    // lives at /events/create
+    @PostMapping("create")
+    public String processCreateEventForm(@ModelAttribute Event newEvent) {
+        EventData.add(newEvent);
+        return "redirect:/events";
+    }
+    @GetMapping("delete")
+    public String displayDeleteEventForm(Model model) {
+        model.addAttribute("title", "Delete Event");
+        model.addAttribute("events", EventData.getAll());
+        return "events/delete";
+    }
 
-            return "index";
+    @PostMapping("delete")
+    public String processDeleteEventsForm(@RequestParam(required = false) int[] eventIds) {
+
+        if (eventIds != null) {
+            for (int id : eventIds) {
+                EventData.remove(id);
+            }
         }
-        //lives at /events /create
-        @GetMapping("create")
-        public String renderCreateEventForm(){
 
-            return "events/create";
-
-        }
+        return "redirect:";
     }
 
 
-
+}
--- Chapter-1/codingevents.zip/src/main/java/org/launchcode/codingevents/zip/controllers/HomeController.java
+++ Chapter-1/codingevents.zip/src/main/java/org/launchcode/codingevents/zip/controllers/HomeController.java
@@ -1,7 +1,4 @@
 package org.launchcode.codingevents.zip.controllers;
 
-import org.springframework.stereotype.Controller;
-
-@Controller
 public class HomeController {
 }
